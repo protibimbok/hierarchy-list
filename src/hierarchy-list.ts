5327794c9e4e4d9eb002a3ede0100fc02680259a
Saad <sakib.saad.khan@gmail.com>
2023-05-26 16:50:23 +0600

    export default

--- src/hierarchy-list.ts
+++ src/hierarchy-list.ts
@@ -1001,7 +1001,7 @@ function addClass(el: HTMLElement, classes: string[]) {
     classes.forEach((name) => el.classList.add(name));
 }
 
-//export default HierarchyList;
+export default HierarchyList;
 //@ts-ignore
 if (window && typeof window.HierarchyList === 'undefined'){
     //@ts-ignore
